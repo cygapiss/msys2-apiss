e8bb076f98f85354a9372f777bb89ae2946b7ea5
Alexpux <alexey.pawlow@gmail.com>
2016-04-04 16:40:37 +0000

    [ports] Merge pull request #530 from Karlson2k/bat_files_01
    Remove Win9x support from .bat files
    
    Source: msys2/MSYS2-packages@6198a2e186eddf81b5491a1d7b49673d1574a6dc

--- ports/filesystem/mingw64_shell.bat
+++ ports/filesystem/mingw64_shell.bat
@@ -1,24 +1,7 @@
 :
 @echo off
 
-rem ember value of GOTO: is used to know recursion has happened.
-if "%1" == "GOTO:" goto %2
-
 if NOT "x%WD%" == "x" set WD=
-
-rem ember command.com only uses the first eight characters of the label.
-goto _WindowsNT
-
-start /min %COMSPEC% /e:4096 /c %0 GOTO: _Resume %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
-goto EOF
-
-rem ember that we execute here if we recursed.
-:_Resume
-for %%F in (1 2 3) do shift
-if NOT EXIST %WD%msys-2.0.dll set WD=.\usr\bin\
-
-rem ember that we get here even in command.com.
-:_WindowsNT
 
 if NOT EXIST %WD%msys-2.0.dll set WD=%~dp0usr\bin\
 
@@ -43,15 +26,15 @@ if "x%MSYSCON%" == "xconsole.exe" goto startconsolez
 :startmintty
 if NOT EXIST %WD%mintty.exe goto startsh
 start %WD%mintty -i /msys2.ico /usr/bin/bash --login %*
-exit /b %ERRORLEVEL%
+exit
 
 :startconsolez
 cd %WD%..\lib\ConsoleZ
 start console -t "MinGW" -r "%*"
-exit /b %ERRORLEVEL%
+exit
 
 :startsh
 start %WD%sh --login -i %*
-exit /b %ERRORLEVEL%
+exit
 
 :EOF
