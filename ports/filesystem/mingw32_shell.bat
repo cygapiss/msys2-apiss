9dfcd68d2d5075b7b14c6cdfe1790c9ff9d2020f
Karlson2k <k2k@narod.ru>
2016-04-04 15:56:12 +0000

    [ports] .bat-files: exit only from .bat file instead of terminating parent command processor,
    bypass result of "start" to calling process
    
    Source: msys2/MSYS2-packages@0fa627dda8b6362c11879c619291875f3f739019

--- ports/filesystem/mingw32_shell.bat
+++ ports/filesystem/mingw32_shell.bat
@@ -1,7 +1,24 @@
 :
 @echo off
 
+rem ember value of GOTO: is used to know recursion has happened.
+if "%1" == "GOTO:" goto %2
+
 if NOT "x%WD%" == "x" set WD=
+
+rem ember command.com only uses the first eight characters of the label.
+goto _WindowsNT
+
+start /min %COMSPEC% /e:4096 /c %0 GOTO: _Resume %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto EOF
+
+rem ember that we execute here if we recursed.
+:_Resume
+for %%F in (1 2 3) do shift
+if NOT EXIST %WD%msys-2.0.dll set WD=.\usr\bin\
+
+rem ember that we get here even in command.com.
+:_WindowsNT
 
 if NOT EXIST %WD%msys-2.0.dll set WD=%~dp0usr\bin\
 
@@ -26,15 +43,15 @@ if "x%MSYSCON%" == "xconsole.exe" goto startconsolez
 :startmintty
 if NOT EXIST %WD%mintty.exe goto startsh
 start %WD%mintty -i /msys2.ico /usr/bin/bash --login %*
-exit
+exit /b %ERRORLEVEL%
 
 :startconsolez
 cd %WD%..\lib\ConsoleZ
 start console -t "MinGW" -r "%*"
-exit
+exit /b %ERRORLEVEL%
 
 :startsh
 start %WD%sh --login -i %*
-exit
+exit /b %ERRORLEVEL%
 
 :EOF
