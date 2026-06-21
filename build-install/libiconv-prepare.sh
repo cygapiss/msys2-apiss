aeb938748e1a2014187dbca6b7164805f78e6083
Yonggang Luo <luoyonggang@gmail.com>
2026-06-22 04:01:31 +0800

    Rename bootstrap stages and use stage for dist paths
    - Rename MSYS_BOOTSTRAP_STAGE values: stage_origin to stage1-rt-origin,
      stage_origin_hook to stage1-rt-hook, stage0 to stage1-core.
    - Use MSYS_BOOTSTRAP_STAGE directly as dist and build-cache directory
      names in build-single.sh; drop DIST_TARGET_DIR_NAME mapping.
    - Require MSYS_BOOTSTRAP_STAGE to be set; remove package-name suffix from
      build-cache paths.
    - Copy built packages to dist/$MSYS_BOOTSTRAP_STAGE/ unless
      MSYS_BOOTSTRAP_DISABLE_COPY_PACKAGES is set.
    - Update build-install scripts, bootstrap-init-stage1.sh, stage-hook.sh,
      stage0.sh, and gen-build-all.mjs for new stage names and dist paths.

--- build-install/libiconv-prepare.sh
+++ build-install/libiconv-prepare.sh
@@ -1,12 +1,12 @@
 echo "Prepare building libiconv as $MSYS_BOOTSTRAP_STAGE"
-if [[ "$MSYS_BOOTSTRAP_STAGE" == "stage0" ]]; then
+if [[ "$MSYS_BOOTSTRAP_STAGE" == "stage1-core" ]]; then
     pushd ${pkg_root_dir}
     set -x
-    # The libtool-bootstrap/msys2-runtime-bootstrap are built at stage0, so they can not be pre-installed, install it here.
-    pacman -U --noconfirm --overwrite \* ./dist/init/libltdl-$LIBTOOL_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst
-    pacman -U --noconfirm --overwrite \* ./dist/init/libtool-$LIBTOOL_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst
-    tar xf ./dist/init/msys2-runtime-$MSYS_RUNTIME_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst -C / usr/bin/cygwin1.dll
-    tar xf ./dist/init/msys2-runtime-devel-$MSYS_RUNTIME_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst -C / usr/lib
+    # The libtool-bootstrap/msys2-runtime-bootstrap are built at stage1-core, so they can not be pre-installed, install it here.
+    pacman -U --noconfirm --overwrite \* ./dist/stage1-core/libltdl-$LIBTOOL_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst
+    pacman -U --noconfirm --overwrite \* ./dist/stage1-core/libtool-$LIBTOOL_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst
+    tar xf ./dist/stage1-core/msys2-runtime-$MSYS_RUNTIME_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst -C / usr/bin/cygwin1.dll
+    tar xf ./dist/stage1-core/msys2-runtime-devel-$MSYS_RUNTIME_PKGVER-$MSYS_RUNTIME_BOOTSTRAP_PKGREL-x86_64.pkg.tar.zst -C / usr/lib
     set +x
     popd
 fi
