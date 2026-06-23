f2dfa95be3f33e58135dfce4dfa523522f35aedc
Yonggang Luo <luoyonggang@gmail.com>
2026-06-23 23:47:02 +0800

    Derive cygperl DLL name from PERL_PKGVER in perl-finish-pkg.sh.
    - Replace hardcoded cygperl5_42_2.dll with PERL_PKGVER dots-to-underscores suffix so perl finish step tracks the configured package version.

--- build-install/perl-finish-pkg.sh
+++ build-install/perl-finish-pkg.sh
@@ -2,5 +2,6 @@ if [[ "$MSYS_BOOTSTRAP_STAGE" == "stage1" ]]; then
     echo "Restore perl package but keep the perl-devel-bootstrap package"
     pacman -U --noconfirm --overwrite \* ${pkg_root_dir}/dist-pkg/perl-$PERL_PKGVER-$PERL_PKGREL-x86_64.pkg.tar.zst
     tar xf ${pkg_root_dir}/ports/perl/perl-tmp.tar -C /
-    cp -arf /usr/bin/cygperl5_42_2.dll /usr/lib/perl5/core_perl/CORE
+    perl_dll_suffix=${PERL_PKGVER//./_}
+    cp -arf /usr/bin/cygperl${perl_dll_suffix}.dll /usr/lib/perl5/core_perl/CORE
 fi
